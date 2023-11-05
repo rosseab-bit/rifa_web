f648b097f1b2b35e7672bb647122e8a5bda1d8a8
hunter <hunter@hunter.hunter.xyx>
2023-11-05 19:16:07 -0300

    conexion con api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import ContextWebState from "./context/ContextWebState";
+import ContextWeb from "./context/ContextWeb";
+import HeadWeb from "./head/HeadWeb";
+import BodyWeb from "./body/Body";
+import FooterWeb from "./footer/FooterWeb";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ContextWebState>
+      <div className="App">
+	<HeadWeb />
+	<BodyWeb />
+	<FooterWeb />
+     </div>
+    </ContextWebState>
+    </>
   );
 }
 
